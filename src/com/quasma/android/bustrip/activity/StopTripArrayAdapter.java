535348aac8bc7986638944f07e6a3d61b9510186
Michael Herrmann <mike@quasma.com>
2014-01-22 16:35:19 -0600

    change actual times for themes

--- src/com/quasma/android/bustrip/activity/StopTripArrayAdapter.java
+++ src/com/quasma/android/bustrip/activity/StopTripArrayAdapter.java
@@ -7,11 +7,11 @@ import com.quasma.android.bustrip.R;
 import com.quasma.android.bustrip.rest.resource.Trip;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnTouchListener;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
@@ -32,21 +32,33 @@ public class StopTripArrayAdapter extends ArrayAdapter
 	{
 		View listItemView;
 		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 		if (values.get(position).isActual())
 		{
 			listItemView =  LayoutInflater.from(context).inflate(R.layout.stoptrips_actual_list_item, parent, false);
+			if (prefs.getBoolean("lightbackground", true))
+				listItemView.setBackgroundColor(getContext().getResources().getColor(R.color.green));
+			else
+				listItemView.setBackgroundColor(getContext().getResources().getColor(android.R.color.background_dark));				
 		}			
 		else
 		{
 			listItemView =  LayoutInflater.from(context).inflate(R.layout.stoptrips_list_item, parent, false);
 		}			
-		
 		((TextView) listItemView.findViewById(R.id.route)).setText(
 				MessageFormat.format(context.getString(R.string.RouteNumber), 
 						values.get(position).getRoute() + values.get(position).getTerminal()));
 		
 		((TextView) listItemView.findViewById(R.id.description)).setText(values.get(position).getDescription());
+		
 		TextView departure = (TextView) listItemView.findViewById(R.id.time);
+		if (values.get(position).isActual())
+		{
+			if (prefs.getBoolean("lightbackground", true))
+				departure.setTextColor(getContext().getResources().getColor(android.R.color.primary_text_light));
+			else
+				departure.setTextColor(getContext().getResources().getColor(R.color.red));
+		}
 		departure.setText(values.get(position).getDeparture());
 		return listItemView;
 	}
--- src/com/quasma/android/bustrip/activity/TripArrayAdapter.java
+++ src/com/quasma/android/bustrip/activity/TripArrayAdapter.java
@@ -7,6 +7,8 @@ import com.quasma.android.bustrip.R;
 import com.quasma.android.bustrip.rest.resource.Trip;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,9 +32,14 @@ public class TripArrayAdapter extends ArrayAdapter
 	{
 		View listItemView;
 		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
 		if (values.get(position).isActual())
 		{
 			listItemView =  LayoutInflater.from(context).inflate(R.layout.stoptrips_actual_list_item, parent, false);
+			if (prefs.getBoolean("lightbackground", true))
+				listItemView.setBackgroundColor(getContext().getResources().getColor(R.color.green));
+			else
+				listItemView.setBackgroundColor(getContext().getResources().getColor(android.R.color.background_dark));				
 		}			
 		else
 		{
@@ -44,7 +51,15 @@ public class TripArrayAdapter extends ArrayAdapter
 						values.get(position).getRoute() + values.get(position).getTerminal()));
 		
 		((TextView) listItemView.findViewById(R.id.description)).setText(values.get(position).getDescription());
+		
 		TextView departure = (TextView) listItemView.findViewById(R.id.time);
+		if (values.get(position).isActual())
+		{
+			if (prefs.getBoolean("lightbackground", true))
+				departure.setTextColor(getContext().getResources().getColor(android.R.color.primary_text_light));
+			else
+				departure.setTextColor(getContext().getResources().getColor(R.color.red));
+		}		
 		departure.setText(values.get(position).getDeparture());
 		return listItemView;
 	}
